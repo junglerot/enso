junglerot/enso

commit cd50a143d9458611da460f5afdfee222b974a6a8

Project Description/Settings sidebar (#7612)
- Closes https://github.com/enso-org/cloud-v2/issues/629
- Project Description/Settings sidebar

# Important Notes
:warning: This is blocked by backend support for asset descriptions.

When the sidebar is opening, there is one jumpy animation:
- The search bar switches from being centered in the screen, to having an equal gap between itself and its siblings

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/dashboard.tsx
@@ -6,6 +6,7 @@ import * as assetEventModule from '../events/assetEvent'
 import * as assetListEventModule from '../events/assetListEvent'
 import * as assetQuery from '../../assetQuery'
 import * as backendModule from '../backend'
+import * as categorySwitcher from './categorySwitcher'
 import * as hooks from '../../hooks'
 import * as http from '../../http'
 import * as localBackendModule from '../localBackend'
@@ -21,9 +22,11 @@ import * as loggerProvider from '../../providers/logger'
 import * as modalProvider from '../../providers/modal'
 import * as shortcutsProvider from '../../providers/shortcuts'
 
+import type * as assetSettingsPanel from './assetSettingsPanel'
 import * as pageSwitcher from './pageSwitcher'
 import type * as spinner from './spinner'
 import Chat, * as chat from './chat'
+import AssetSettingsPanel from './assetSettingsPanel'
 import Drive from './drive'
 import Editor from './editor'
 import Home from './home'
@@ -77,6 +80,13 @@ export default function Dashboard(props: DashboardProps) {
     const [assetListEvents, dispatchAssetListEvent] =
         hooks.useEvent<assetListEventModule.AssetListEvent>()
     const [assetEvents, dispatchAssetEvent] = hooks.useEvent<assetEventModule.AssetEvent>()
+    const [assetSettingsPanelProps, setAssetSettingsPanelProps] =
+        React.useState<assetSettingsPanel.AssetSettingsPanelRequiredProps | null>(null)
+    const [isAssetSettingsPanelVisible, setIsAssetSettingsPanelVisible] = React.useState(
+        () =>
+            localStorage.get(localStorageModule.LocalStorageKey.isAssetSettingsPanelVisible) ??
+            false
+    )
     const [initialProjectName, setInitialProjectName] = React.useState(rawInitialProjectName)
 
     const isListingLocalDirectoryAndWillFail =
@@ -237,6 +247,13 @@ export default function Dashboard(props: DashboardProps) {
     }, [projectStartupInfo, /* should never change */ localStorage])
 
     React.useEffect(() => {
+        localStorage.set(
+            localStorageModule.LocalStorageKey.isAssetSettingsPanelVisible,
+            isAssetSettingsPanelVisible
+        )
+    }, [isAssetSettingsPanelVisible, /* should never change */ localStorage])
+
+    React.useEffect(() => {
         localStorage.set(localStorageModule.LocalStorageKey.page, page)
     }, [page, /* should never change */ localStorage])
 
@@ -366,83 +383,128 @@ export default function Dashboard(props: DashboardProps) {
         )
     }, [])
 
+    const doRemoveSelf = React.useCallback(() => {
+        if (projectStartupInfo?.projectAsset != null) {
+            dispatchAssetListEvent({
+                type: assetListEventModule.AssetListEventType.removeSelf,
+                id: projectStartupInfo.projectAsset.id,
+            })
+            setProjectStartupInfo(null)
+        }
+    }, [projectStartupInfo?.projectAsset, /* should never change */ dispatchAssetListEvent])
+
+    const onSignOut = React.useCallback(() => {
+        if (page === pageSwitcher.Page.editor) {
+            setPage(pageSwitcher.Page.drive)
+        }
+        setProjectStartupInfo(null)
+    }, [page])
+
     return (
         <>
-            <div
-                className={`flex flex-col relative select-none text-primary text-xs h-screen pb-2 ${
-                    page === pageSwitcher.Page.editor ? 'cursor-none pointer-events-none' : ''
-                }`}
-                onContextMenu={event => {
-                    event.preventDefault()
-                    unsetModal()
-                }}
-            >
-                <TopBar
-                    supportsLocalBackend={supportsLocalBackend}
-                    projectAsset={projectStartupInfo?.projectAsset ?? null}
-                    setProjectAsset={projectStartupInfo?.setProjectAsset ?? null}
-                    page={page}
-                    setPage={setPage}
-                    asset={null}
-                    isEditorDisabled={projectStartupInfo == null}
-                    isHelpChatOpen={isHelpChatOpen}
-                    setIsHelpChatOpen={setIsHelpChatOpen}
-                    setBackendType={setBackendType}
-                    query={query}
-                    setQuery={setQuery}
-                    doRemoveSelf={() => {
-                        if (projectStartupInfo?.projectAsset != null) {
-                            dispatchAssetListEvent({
-                                type: assetListEventModule.AssetListEventType.removeSelf,
-                                id: projectStartupInfo.projectAsset.id,
-                            })
+            <div className="flex text-primary text-xs">
+                <div
+                    className={`flex flex-col grow container-size gap-2 overflow-hidden relative select-none h-screen pb-2 ${
+                        page === pageSwitcher.Page.editor ? 'cursor-none pointer-events-none' : ''
+                    }`}
+                    onContextMenu={event => {
+                        event.preventDefault()
+                        unsetModal()
+                    }}
+                >
+                    <TopBar
+                        supportsLocalBackend={supportsLocalBackend}
+                        projectAsset={projectStartupInfo?.projectAsset ?? null}
+                        setProjectAsset={projectStartupInfo?.setProjectAsset ?? null}
+                        page={page}
+                        setPage={setPage}
+                        isEditorDisabled={projectStartupInfo == null}
+                        isHelpChatOpen={isHelpChatOpen}
+                        setIsHelpChatOpen={setIsHelpChatOpen}
+                        setBackendType={setBackendType}
+                        query={query}
+                        setQuery={setQuery}
+                        canToggleSettingsPanel={assetSettingsPanelProps != null}
+                        isSettingsPanelVisible={
+                            isAssetSettingsPanelVisible && assetSettingsPanelProps != null
+                        }
+                        setIsSettingsPanelVisible={setIsAssetSettingsPanelVisible}
+                        doRemoveSelf={doRemoveSelf}
+                        onSignOut={() => {
+                            if (page === pageSwitcher.Page.editor) {
+                                setPage(pageSwitcher.Page.drive)
+                            }
                             setProjectStartupInfo(null)
-                        }
-                    }}
-                    onSignOut={() => {
-                        if (page === pageSwitcher.Page.editor) {
-                            setPage(pageSwitcher.Page.drive)
-                        }
-                        setProjectStartupInfo(null)
-                    }}
-                />
-                <Home hidden={page !== pageSwitcher.Page.home} onTemplateClick={doCreateProject} />
-                <Drive
-                    supportsLocalBackend={supportsLocalBackend}
-                    hidden={page !== pageSwitcher.Page.drive}
-                    page={page}
-                    initialProjectName={initialProjectName}
-                    query={query}
-                    setQuery={setQuery}
-                    projectStartupInfo={projectStartupInfo}
-                    queuedAssetEvents={queuedAssetEvents}
-                    assetListEvents={assetListEvents}
-                    dispatchAssetListEvent={dispatchAssetListEvent}
-                    assetEvents={assetEvents}
-                    dispatchAssetEvent={dispatchAssetEvent}
-                    doCreateProject={doCreateProject}
-                    doOpenEditor={openEditor}
-                    doCloseEditor={closeEditor}
-                    loadingProjectManagerDidFail={loadingProjectManagerDidFail}
-                    isListingRemoteDirectoryWhileOffline={isListingRemoteDirectoryWhileOffline}
-                    isListingLocalDirectoryAndWillFail={isListingLocalDirectoryAndWillFail}
-                    isListingRemoteDirectoryAndWillFail={isListingRemoteDirectoryAndWillFail}
-                />
-                <Editor
-                    hidden={page !== pageSwitcher.Page.editor}
-                    supportsLocalBackend={supportsLocalBackend}
-                    projectStartupInfo={projectStartupInfo}
-                    appRunner={appRunner}
-                />
-                {/* `session.accessToken` MUST be present in order for the `Chat` component to work. */}
-                {isHelpChatVisible && session.accessToken != null && (
-                    <Chat
-                        isOpen={isHelpChatOpen}
-                        doClose={() => {
-                            setIsHelpChatOpen(false)
                         }}
                     />
-                )}
+                    <Home
+                        hidden={page !== pageSwitcher.Page.home}
+                        onTemplateClick={doCreateProject}
+                    />
+                    <Drive
+                        supportsLocalBackend={supportsLocalBackend}
+                        hidden={page !== pageSwitcher.Page.drive}
+                        page={page}
+                        initialProjectName={initialProjectName}
+                        query={query}
+                        setQuery={setQuery}
+                        projectStartupInfo={projectStartupInfo}
+                        queuedAssetEvents={queuedAssetEvents}
+                        assetListEvents={assetListEvents}
+                        dispatchAssetListEvent={dispatchAssetListEvent}
+                        assetEvents={assetEvents}
+                        dispatchAssetEvent={dispatchAssetEvent}
+                        setAssetSettingsPanelProps={setAssetSettingsPanelProps}
+                        doCreateProject={doCreateProject}
+                        doOpenEditor={openEditor}
+                        doCloseEditor={closeEditor}
+                        loadingProjectManagerDidFail={loadingProjectManagerDidFail}
+                        isListingRemoteDirectoryWhileOffline={isListingRemoteDirectoryWhileOffline}
+                        isListingLocalDirectoryAndWillFail={isListingLocalDirectoryAndWillFail}
+                        isListingRemoteDirectoryAndWillFail={isListingRemoteDirectoryAndWillFail}
+                    />
+                    <Editor
+                        hidden={page !== pageSwitcher.Page.editor}
+                        supportsLocalBackend={supportsLocalBackend}
+                        projectStartupInfo={projectStartupInfo}
+                        appRunner={appRunner}
+                    />
+                    {/* `session.accessToken` MUST be present in order for the `Chat` component to work. */}
+                    {isHelpChatVisible && session.accessToken != null && (
+                        <Chat
+                            page={page}
+                            isOpen={isHelpChatOpen}
+                            doClose={() => {
+                                setIsHelpChatOpen(false)
+                            }}
+                        />
+                    )}
+                </div>
+                <div
+                    className={`flex flex-col duration-500 transition-min-width ease-in-out overflow-hidden ${
+                        isAssetSettingsPanelVisible && assetSettingsPanelProps != null
+                            ? 'min-w-120'
+                            : 'min-w-0'
+                    }`}
+                >
+                    {assetSettingsPanelProps && (
+                        <AssetSettingsPanel
+                            supportsLocalBackend={supportsLocalBackend}
+                            key={assetSettingsPanelProps.item.item.id}
+                            {...assetSettingsPanelProps}
+                            page={page}
+                            category={categorySwitcher.Category.home}
+                            isHelpChatOpen={isHelpChatOpen}
+                            setIsHelpChatOpen={setIsHelpChatOpen}
+                            setIsSettingsPanelVisible={setIsAssetSettingsPanelVisible}
+                            dispatchAssetEvent={dispatchAssetEvent}
+                            projectAsset={projectStartupInfo?.projectAsset ?? null}
+                            setProjectAsset={projectStartupInfo?.setProjectAsset ?? null}
+                            doRemoveSelf={doRemoveSelf}
+                            onSignOut={onSignOut}
+                        />
+                    )}
+                </div>
             </div>
             <div className="text-xs text-primary select-none">
                 <TheModal />

app/ide-desktop/eslint.config.js
@@ -30,7 +30,7 @@ const NAME = 'enso'
  * `yargs` is a modules we explicitly want the default imports of.
  * `node:process` is here because `process.on` does not exist on the namespace import. */
 const DEFAULT_IMPORT_ONLY_MODULES =
-    'node:process|chalk|string-length|yargs|yargs\\u002Fyargs|sharp|to-ico|connect|morgan|serve-static|create-servers|electron-is-dev|fast-glob|esbuild-plugin-.+|opener|tailwindcss.*|enso-assets.*|@modyfi\\u002Fvite-plugin-yaml'
+    'node:process|chalk|string-length|yargs|yargs\\u002Fyargs|sharp|to-ico|connect|morgan|serve-static|create-servers|electron-is-dev|fast-glob|esbuild-plugin-.+|opener|tailwindcss.*|enso-assets.*|@modyfi\\u002Fvite-plugin-yaml|validator.+'
 const ALLOWED_DEFAULT_IMPORT_MODULES = `${DEFAULT_IMPORT_ONLY_MODULES}|postcss`
 const OUR_MODULES = 'enso-.*'
 const RELATIVE_MODULES =

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/backend.ts
@@ -446,6 +446,7 @@ export interface BaseAsset {
     parentId: DirectoryId
     permissions: UserPermission[] | null
     labels: LabelName[] | null
+    description: string | null
 }
 
 /** Metadata uniquely identifying a directory entry.
@@ -483,6 +484,7 @@ export function createSpecialLoadingAsset(directoryId: DirectoryId): SpecialLoad
         permissions: [],
         projectState: null,
         labels: [],
+        description: null,
     }
 }
 
@@ -501,6 +503,7 @@ export function createSpecialEmptyAsset(directoryId: DirectoryId): SpecialEmptyA
         permissions: [],
         projectState: null,
         labels: [],
+        description: null,
     }
 }
 
@@ -600,7 +603,8 @@ export interface UpdateDirectoryRequestBody {
 
 /** HTTP request body for the "update asset" endpoint. */
 export interface UpdateAssetRequestBody {
-    parentDirectoryId: DirectoryId
+    parentDirectoryId: DirectoryId | null
+    description: string | null
 }
 
 /** HTTP request body for the "create project" endpoint. */

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/column.tsx
@@ -160,8 +160,19 @@ function LastModifiedColumn(props: AssetColumnProps) {
 // === SharedWithColumn ===
 // ========================
 
+/** The type of the `state` prop of a {@link SharedWithColumn}. */
+interface SharedWithColumnStateProp {
+    category: AssetColumnProps['state']['category']
+    dispatchAssetEvent: AssetColumnProps['state']['dispatchAssetEvent']
+}
+
+/** Props for a {@link SharedWithColumn}. */
+export interface SharedWithColumnProps extends Pick<AssetColumnProps, 'item' | 'setItem'> {
+    state: SharedWithColumnStateProp
+}
+
 /** A column listing the users with which this asset is shared. */
-function SharedWithColumn(props: AssetColumnProps) {
+export function SharedWithColumn(props: SharedWithColumnProps) {
     const {
         item: { item: asset },
         setItem,
@@ -371,6 +382,14 @@ function LabelsColumn(props: AssetColumnProps) {
     )
 }
 
+/** A column listing the users with which this asset is shared. */
+export function DocsColumn(props: AssetColumnProps) {
+    const {
+        item: { item: asset },
+    } = props
+    return <div className="flex items-center gap-1">{asset.description}</div>
+}
+
 // =========================
 // === PlaceholderColumn ===
 // =========================
@@ -409,7 +428,8 @@ export const COLUMN_HEADING: Record<
                 onMouseLeave={() => {
                     setIsHovered(false)
                 }}
-                onClick={() => {
+                onClick={event => {
+                    event.stopPropagation()
                     if (sortColumn === Column.name) {
                         setSortDirection(sorting.NEXT_SORT_DIRECTION[sortDirection ?? 'null'])
                     } else {
@@ -441,7 +461,8 @@ export const COLUMN_HEADING: Record<
                 onMouseLeave={() => {
                     setIsHovered(false)
                 }}
-                onClick={() => {
+                onClick={event => {
+                    event.stopPropagation()
                     if (sortColumn === Column.modified) {
                         setSortDirection(sorting.NEXT_SORT_DIRECTION[sortDirection ?? 'null'])
                     } else {
@@ -503,5 +524,5 @@ export const COLUMN_RENDERER: Record<Column, (props: AssetColumnProps) => JSX.El
     [Column.labels]: LabelsColumn,
     [Column.accessedByProjects]: PlaceholderColumn,
     [Column.accessedData]: PlaceholderColumn,
-    [Column.docs]: PlaceholderColumn,
+    [Column.docs]: DocsColumn,
 }

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/assetInfoBar.tsx
@@ -4,20 +4,33 @@ import * as React from 'react'
 import DocsIcon from 'enso-assets/docs.svg'
 import SettingsIcon from 'enso-assets/settings.svg'
 
-import type * as backend from '../backend'
+import * as backendModule from '../backend'
+import * as backendProvider from '../../providers/backend'
+
 import Button from './button'
 
 /** Props for an {@link AssetInfoBar}. */
 export interface AssetInfoBarProps {
-    asset: backend.Asset | null
+    canToggleSettingsPanel: boolean
+    isSettingsPanelVisible: boolean
+    setIsSettingsPanelVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 /** A toolbar for displaying asset information. */
 // This parameter will be used in the future.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function AssetInfoBar(_props: AssetInfoBarProps) {
+export default function AssetInfoBar(props: AssetInfoBarProps) {
+    const { canToggleSettingsPanel, isSettingsPanelVisible, setIsSettingsPanelVisible } = props
+    const { backend } = backendProvider.useBackend()
     return (
-        <div className="flex items-center shrink-0 bg-frame backdrop-blur-3xl rounded-full gap-3 h-8 px-2 cursor-default pointer-events-auto">
+        <div
+            className={`flex items-center shrink-0 bg-frame rounded-full gap-3 h-8 px-2 cursor-default pointer-events-auto ${
+                backend.type === backendModule.BackendType.remote ? '' : 'invisible'
+            }`}
+            onClick={event => {
+                event.stopPropagation()
+            }}
+        >
             <Button
                 active={false}
                 disabled
@@ -28,12 +41,14 @@ export default function AssetInfoBar(_props: AssetInfoBarProps) {
                 }}
             />
             <Button
-                active={false}
-                disabled
+                active={canToggleSettingsPanel && isSettingsPanelVisible}
+                disabled={!canToggleSettingsPanel}
                 image={SettingsIcon}
-                error="Not implemented yet."
+                error="Select exactly one asset to see its settings."
                 onClick={() => {
-                    // No backend support yet.
+                    setIsSettingsPanelVisible(
+                        oldIsSettingsPanelVisible => !oldIsSettingsPanelVisible
+                    )
                 }}
             />
         </div>

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/assetRow.tsx
@@ -46,13 +46,21 @@ export default function AssetRow(props: AssetRowProps) {
         initialRowState,
         hidden,
         selected,
+        isSoleSelectedItem,
         setSelected,
         allowContextMenu,
         onContextMenu,
         state,
         columns,
     } = props
-    const { assetEvents, dispatchAssetEvent, dispatchAssetListEvent, doCut, doPaste } = state
+    const {
+        assetEvents,
+        dispatchAssetEvent,
+        dispatchAssetListEvent,
+        setAssetSettingsPanelProps,
+        doCut,
+        doPaste,
+    } = state
     const { organization } = authProvider.useNonPartialUserSession()
     const { backend } = backendProvider.useBackend()
     const { setModal, unsetModal } = modalProvider.useSetModal()
@@ -66,6 +74,7 @@ export default function AssetRow(props: AssetRowProps) {
         ...initialRowState,
         setVisibility,
     }))
+
     React.useEffect(() => {
         setItem(rawItem)
     }, [rawItem])
@@ -104,7 +113,10 @@ export default function AssetRow(props: AssetRowProps) {
                 }))
                 await backend.updateAsset(
                     asset.id,
-                    { parentDirectoryId: newParentId ?? backend.rootDirectoryId(organization) },
+                    {
+                        parentDirectoryId: newParentId ?? backend.rootDirectoryId(organization),
+                        description: null,
+                    },
                     asset.title
                 )
             } catch (error) {
@@ -135,6 +147,12 @@ export default function AssetRow(props: AssetRowProps) {
             /* should never change */ dispatchAssetListEvent,
         ]
     )
+
+    React.useEffect(() => {
+        if (isSoleSelectedItem) {
+            setAssetSettingsPanelProps({ item, setItem })
+        }
+    }, [item, isSoleSelectedItem, /* should never change */ setAssetSettingsPanelProps])
 
     const doDelete = React.useCallback(async () => {
         setVisibility(visibilityModule.Visibility.hidden)

commit 09f11f76a31637d226cb703b703433d52dd1c53f

Fix GUI2 not being interactable (#8434)
- Broken by #7612, which restructured the JSX markup for the main Dashboard component.
- Should affect (and be reproducible) on GUI1 as well.

# Important Notes
None

app/ide-desktop/lib/dashboard/src/authentication/src/dashboard/components/dashboard.tsx
@@ -402,11 +402,13 @@ export default function Dashboard(props: DashboardProps) {
 
     return (
         <>
-            <div className="flex text-primary text-xs">
+            <div
+                className={`flex text-primary text-xs ${
+                    page === pageSwitcher.Page.editor ? 'cursor-none pointer-events-none' : ''
+                }`}
+            >
                 <div
-                    className={`flex flex-col grow container-size gap-2 overflow-hidden relative select-none h-screen pb-2 ${
-                        page === pageSwitcher.Page.editor ? 'cursor-none pointer-events-none' : ''
-                    }`}
+                    className="flex flex-col grow container-size gap-2 overflow-hidden relative select-none h-screen pb-2"
                     onContextMenu={event => {
                         event.preventDefault()
                         unsetModal()